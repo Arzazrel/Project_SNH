8fd602538a98666dd6b9fb26bd6351e62688b1ea
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2026-05-18 17:24:59 +0200

    Update security_utils.php

--- Code/var/www/musicwave/includes/security_utils.php
+++ Code/var/www/musicwave/includes/security_utils.php
@@ -13,13 +13,12 @@ class SecurityUtils {
      * Prevents Null Byte Injection attacks.
      */
     public static function sanitizeInput($data) {
-    	// check if the input is an array
         if (is_array($data)) {
-            return array_map([self::class, 'sanitizeInput'], $data);	// (e.g., a form with checkboxes or multiple fields) apply the function recursively to each element of the array.
+            return array_map([self::class, 'sanitizeInput'], $data);
         }
-        
-        $data = str_replace(chr(0), '', $data);		// Remove Null Byte (0x00)
-        return trim($data);				// Removes blank spaces, tabs, or line breaks at the beginning and end of the string.
+        // Remove Null Byte (0x00)
+        $data = str_replace(chr(0), '', $data);
+        return trim($data);
     }
 
     /**
@@ -38,17 +37,60 @@ class SecurityUtils {
     }
 
     /**
-     * Email validation for username.
+     * Username validation 
+     */
+    public static function validateUsername($username) {
+        $clean = self::sanitizeInput($username);
+        // lenght 3-20, alphanumeric and underscore only
+        if (!preg_match("/^[a-zA-Z0-9_]{3,20}$/", $clean)) {
+            global $securityLogger;
+            $securityLogger->warning("Invalid username format", ["input" => $username]);
+            return false;
+        }
+        return $username;
+    }
+
+    /**
+     * Email validation.
      * Logs a warning if the format is invalid.
      */
     public static function validateEmail($email) {
-        $email = self::sanitizeInput($email);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $clean = self::sanitizeInput($email);
+        if (strlen($clean) > 255 || !filter_var($clean, FILTER_VALIDATE_EMAIL)) {
             global $securityLogger;
             $securityLogger->warning("Invalid email format for username", ["email" => $email]);
             return false;
         }
         return $email;
     }
+    
+    /**
+     * Password validation. Use PHP PASSWORD_BCRYPT: password parameter being truncated to a maximum length of 72 bytes. 
+     * First lenght check and then security level check. To achieve a strong password at least one uppercase, one lowercase, one number, and one special char.
+     * Logs a warning if the format is invalid.
+     */
+    public static function validatePassword($password) {
+	    // length check
+	    $length = strlen($password);
+	    if ($length < 8 || $length > 72) {
+		global $securityLogger;
+		$securityLogger->warning("Password validation failed: invalid length", ["length" => $length]);
+		return false;
+	    }
+	    /*
+	    // 2. Regex per la complessità:
+	    // (?=.*[a-z]) -> at least a lower case
+	    // (?=.*[A-Z]) -> at least an upper case
+	    // (?=.*\d)    -> at least one number
+	    // (?=.*[\W_]) -> at least an special char (non alfanumerico)
+	    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/', $password)) {
+		global $securityLogger;
+		$securityLogger->warning("Password validation failed: weak complexity");
+		return false;
+	    }*/
+
+    	return true; // La password è valida
+    }
+    
 }
 ?>
